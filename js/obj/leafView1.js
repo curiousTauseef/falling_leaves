c497499bc68a3546b543522446615ed6942e19ec
Gabriel Harp <gabrielharp@GHarp.local>
2012-03-03 16:47:02 -0800

    adding leaf one

---
+++ js/obj/leafView1.js
@@ -0,0 +1,49 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "generatedBy"   : "Blender 2.62 Exporter",
+        "vertices"      : 4,
+        "faces"         : 1,
+        "normals"       : 2,
+        "colors"        : 0,
+        "uvs"           : 4,
+        "materials"     : 1,
+        "morphTargets"  : 0
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Material.001",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.6400000190734865, 0.6400000190734865, 0.6400000190734865],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"mapDiffuse" : "leaf_mask_texture_00001.jpg",
+	"mapDiffuseWrap" : ["repeat", "repeat"],
+	"mapLight" : "leaf_texture_00000.jpg",
+	"mapLightWrap" : ["repeat", "repeat"],
+	"shading" : "Lambert",
+	"specularCoef" : 50,
+	"transparency" : 0.0,
+	"vertexColors" : false
+	}],
+
+    "vertices": [1.000000,-0.000000,-1.000000,1.000000,0.000000,1.000000,-1.000000,0.000000,1.000000,-1.000000,-0.000000,-1.000000],
+
+    "morphTargets": [],
+
+    "normals": [0.000000,0.999969,0.000000,0.000000,1.000000,0.000000],
+
+    "colors": [],
+
+    "uvs": [[0.000000,1.000000,1.000000,1.000000,1.000000,0.000000,0.000000,0.000000]],
+
+    "faces": [43,0,3,2,1,0,0,1,2,3,0,1,1,1]
+
+
+
+}
